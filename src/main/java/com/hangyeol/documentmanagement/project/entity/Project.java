4002f24bd3fb3e7c78c48b407ea17ba5c300c02f
21g <leehan0311@gmail.com>
2026-08-19 15:27:25 +0900

    feat: 프로젝트 엔티티 추가
    프로젝트 엔티티 추가, FK를 위한 임시 User 엔티티 생성

---
+++ src/main/java/com/hangyeol/documentmanagement/project/entity/Project.java
@@ -0,0 +1,26 @@
+package com.hangyeol.documentmanagement.project.entity;
+
+import com.hangyeol.documentmanagement.user.entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User owner;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/hangyeol/documentmanagement/user/entity/User.java
@@ -0,0 +1,4 @@
+package com.hangyeol.documentmanagement.user.entity;
+
+public class User {
+}
